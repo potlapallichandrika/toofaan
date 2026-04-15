dee4c9ae29a7aeb76485a283b5a189982f6fcc13
potlapallichandrika <chandrikaip4@gmail.com>
2026-04-15 11:45:23 +0530

    exit remove

--- local/teacher/testcenter/sub_list.php
+++ local/teacher/testcenter/sub_list.php
@@ -169,7 +169,7 @@ $acivitystopdate=0;
 echo 'actid = ' . $actid . '<br>';
 echo 'typeid = ' . $typeid . '<br>';
 echo 'h5pactivity type id = ' . $activityTypeIds['h5pactivity'] . '<br>';
-exit;
+
 
 		if($DB->get_field('activity_status_tsl','status',  array('activityid' => $actid))){
                         $result=$DB->get_field('activity_status_tsl','status',  array('activityid' => $actid));
